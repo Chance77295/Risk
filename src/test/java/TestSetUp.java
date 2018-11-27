ecd2eb5b364298082b4ae9ee4afa0af013d52e8c
chance <chancemorris77295@gmail.com>
2018-11-27 15:53:11 -0600

    Claim Countries Test 2

--- src/test/java/TestSetUp.java
+++ src/test/java/TestSetUp.java
@@ -40,6 +40,17 @@ public class TestSetUp {
   @Test
   public void testClaimCountry () {
     setUpGame game = new setUpGame();
+    final String[] countries = {"Alaska","Alberta","Central America","Eastern United States",
+								"Greenland","Northwest Territory","Ontario","Quebec","Western United States",
+								"Argentina","Brazil","Venezuela","Great Britain","Iceland","Northern Europe",
+								"Scandinavia","Southern Europe","Ukraine","Western Europe","Congo","East Africa",
+								"Egypt","Madagascar","North Africa","South Africa","Afghanistan","China","India",
+								"Irkutsk","Japan","Kamchatka","Middle East","Mongolia","Siam","Siberia","Ural",
+                "Yakutsk","Eastern Australia","Indonesia","LotR","New Guinea","Western Australia"};
+    final String[] continents = {};
+    final String[] borders = {};
+    RiskBoard board = new RiskBoard();
+    board.SetBoard(countries, continents, borders);
     game.claimCountry(true);
 
   }
