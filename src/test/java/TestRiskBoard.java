e6cf131e62c6a33f2c0957d42619762c6b1c537f
chance <chancemorris77295@gmail.com>
2018-11-29 12:15:42 -0600

    Test RiskBoard 2

--- src/test/java/TestRiskBoard.java
+++ src/test/java/TestRiskBoard.java
@@ -17,5 +17,31 @@ public class TestRiskBoard {
         final String[] borders = {};
         RiskBoard board = new RiskBoard();
         board.SetBoard(countries, continents, borders);
+        board.returnCountries();
+        board.returnNameOfCountry("China");
+        board.returnContinents();
+        board.returnNameOfContinent("North America");
+  }
+  
+  @Test
+  public void testSetArmy() {
+    setUpGame game = new setUpGame();
+        final String[] countries = {"Alaska","Alberta","Central America","Eastern United States",
+                                    "Greenland","Northwest Territory","Ontario","Quebec","Western United States",
+                                    "Argentina","Brazil","Venezuela","Great Britain","Iceland","Northern Europe",
+                                    "Scandinavia","Southern Europe","Ukraine","Western Europe","Congo","East Africa",
+                                    "Egypt","Madagascar","North Africa","South Africa","Afghanistan","China","India",
+                                    "Irkutsk","Japan","Kamchatka","Middle East","Mongolia","Siam","Siberia","Ural",
+                    "Yakutsk","Eastern Australia","Indonesia","LotR","New Guinea","Western Australia"};
+        final String[] continents = {"North America","South America", "Australia", "Asia", "Africa", "Europe"};
+        final String[] borders = {};
+        Players player = new Players("Georgy",30);
+        RiskBoard board = new RiskBoard();
+        board.SetBoard(countries, continents, borders);
+        board.setPlayer("China", player);
+        board.returnPlayer("China");
+        board.setNumOfArmies("China", 5);
+        board.returnNumOfArmies("China");
+
   }
 }
