83dd3d2c4eac89dc0b9dd47c3a1bd281c2814541
chance <chancemorris77295@gmail.com>
2018-12-02 16:09:09 -0600

    TestMain 1

---
+++ src/test/java/TestMain.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import twitter4j.TwitterException;
+import java.io.IOException;
+
+public class TestMain {
+
+    @Test
+    public void testMain(String[] args) throws TwitterException, IOException {
+
+    }
+
+}
